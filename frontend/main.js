35663a53b91f07e6e80eb3abd18698b59940404d
Arnaud FERRIERE <106444914+Arnaud-Ferriere@users.noreply.github.com>
2024-08-11 17:38:46 +0200

    Adding the counter

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,19 @@
-const functionApi ='';
+window.addEventListener('DOMContentLoaded',(event) =>{
+    getVisitCount();
+})
+
+const functionApi ='http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(Response => {
         return Response.json()
-    }).then
+    }).then(response =>{
+        console.log("FunctionApi has been called");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
 }
